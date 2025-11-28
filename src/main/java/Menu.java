6b13b564067b250fac9a5c712b360bfa8d6d0134
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2025-11-28 15:15:57 +0100

    Ajout des fichiers de base

---
+++ src/main/java/Menu.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Menu {
+
+	
+	
+	public Menu() {
+		
+	}
+	
+	
+	public void menu() {
+		
+		boolean continuer = true;
+		while (continuer) {
+			System.out.println("");
+			System.out.println("");
+			System.out.println("<<<<<<<  MENU   >>>>>>>>");
+			System.out.println("1. Afficher tous les programmeurs");
+			System.out.println("2. Afficher un programmeur");
+			System.out.println("3. Supprimer un programmeur");
+			System.out.println("4. Ajouter un programmeur");
+			System.out.println("5. Modifier un salaire");
+			System.out.println("6. Afficher la liste des projets (Intitulé, Date de début, Date de fin prévue, Etat, …)");
+			System.out.println("7. Obtenir la liste des programmeurs qui travaillent sur le même projet");
+			System.out.println("8. Quitter");
+			System.out.println("");
+			System.out.println("");
+			Scanner scMenue = new Scanner(System.in);
+			int action = scMenue.nextInt();
+			
+			switch(action) {
+				case 1 :
+					System.out.println("choix 1");
+					break;
+					
+				case 2 :
+					System.out.println("choix 2");
+					break;
+					
+				case 3 :
+					System.out.println("choix 3");
+					break;
+					
+				case 4 :
+					System.out.println("choix 4");
+					break;
+					
+				case 5 :
+					System.out.println("choix 5");
+					break;
+				
+				case 6 :
+					System.out.println("choix 6");
+					break;
+					
+				case 7 :
+					System.out.println("choix 7");
+					break;
+					
+				case 8 :
+					System.out.println("choix 8");
+					continuer = false;
+					System.out.println("Au revoir :)");
+					break;
+			}
+		}
+			
+	}
+	
+}
---
+++ src/main/java/Start.java
@@ -0,0 +1,10 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.menu();
+
+	}
+
+}
